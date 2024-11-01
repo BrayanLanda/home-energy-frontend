cf422abc9a834de1ab52ccc822d9f7bf02f19287
Brayan Acosta <landakkpp@gmail.com>
2024-11-01 00:49:09 -0500

    add user name in sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AccountService } from '../../services/account.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,7 +10,17 @@ import { RouterLink } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  private accountService = inject(AccountService);
   isCollapsed = false;
+
+  // Acceder al usuario actual a través del signal
+  currentUser = this.accountService.currentUser;
+
+  // Función para capitalizar la primera letra
+  capitalizeFirstLetter(str: string): string {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { User } from '../models/user';
 import { LoginRequest } from '../models/login-request';
 import { LoginResponse } from '../models/login-response';
 import { UpdateUserRequest } from '../models/update-user-request';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +14,38 @@ export class AccountService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
 
-  constructor() { }
+  // Signal para mantener el estado del usuario actual
+  currentUser = signal<LoginResponse | null>(null);
+
+  constructor() {
+    // Intentar recuperar usuario del localStorage al iniciar
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.currentUser.set(JSON.parse(storedUser));
+    }
+   }
 
   register(user: User){
     return this.http.post<User>(`${this.baseUrl}user/register`, user);
   }
 
   login(credentials: LoginRequest) {
-    return this.http.post<LoginResponse>(`${this.baseUrl}user/login`, credentials);
+    return this.http.post<LoginResponse>(`${this.baseUrl}user/login`, credentials)
+    .pipe(
+      tap(response => {
+        // Guardar usuario en signal y localStorage
+        this.currentUser.set(response);
+        localStorage.setItem('user', JSON.stringify(response));
+      })
+    );
   }
 
   updateUser(userId: number, userData: UpdateUserRequest) {
     return this.http.put<LoginResponse>(`${this.baseUrl}user/${userId}`, userData);
+  }
+
+  logout() {
+    this.currentUser.set(null);
+    localStorage.removeItem('user');
   }
 }
